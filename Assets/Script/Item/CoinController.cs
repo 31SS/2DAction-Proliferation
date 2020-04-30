33301873c9a13f0eb499dcc58697eebdf0e747ef
SaitoSS <sato991105@icloud.com>
2020-04-30 15:37:07 +0900

    ブランチ作成前　ファイル整理

---
+++ Assets/Script/Item/CoinController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour , IPickupable
+{
+    public AudioClip getCoin;
+
+    public void PickedUp(UnityChan2DController player)
+    {
+        PointController.instance.AddCoin();
+        AudioSourceController.instance.PlayOneShot(getCoin);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/Item/IncreaceItem.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class IncreaceItem : MonoBehaviour, IPickupable {
+
+    public GameObject originPlayer;
+    public void PickedUp(UnityChan2DController player)
+    {
+        GetComponent<Collider2D>().enabled = false;
+        Invoke("Increace", 0.5f);
+    }
+
+    private void Increace()
+    {
+        Instantiate(originPlayer, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+    }
+}
